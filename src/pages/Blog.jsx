53eb50b50e40289dd9be9ea509efc734d043b06d
UIT19520358 <19520358@gm.uit.edu.vn>
2022-04-02 23:52:08 +0700

    Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,10 +8,57 @@ const Blog = ()=>{
     return (
         <>
 
-        <h1>blog</h1>
-        <p>
-            dddasdddas
-        </p>
+        <div class="container mx-auto space-y-4">
+            <h1 className='text-[#003663] text-[31px] py-4 font-bold'>Blog</h1>
+            <div class="individual">
+                <div>
+                <img src="../assets/Ellipse12.png" alt="" />
+                <p className='text-[black] text-[14px]'>Johnny Nguyen</p>
+                </div>
+                <h2 class='content'>Time and motivation</h2>
+                <p className='text-[16px]'>Maybe it's been a long time since I've touched a thing called "timetable". Or more rustic, 
+                people often call it "daily schedule", and for students, it is called "timetable". Surely many 
+                people will wonder why I am a student and have not touched what students call a timetable for a
+                long time. Of course, students or students all need it to see the class schedule, so that not 
+                a single morning is forgotten...</p>
+            </div>
+            <div class="individual">
+                <div>
+                <img src="../assets/Ellipse12.png" alt="" />
+                <p className='text-[black] text-[14px]'>Johnny Nguyen</p>
+                </div>
+                <h2 class='content'>Time and motivation</h2>
+                <p className='text-[16px]'>Maybe it's been a long time since I've touched a thing called "timetable". Or more rustic, 
+                people often call it "daily schedule", and for students, it is called "timetable". Surely many 
+                people will wonder why I am a student and have not touched what students call a timetable for a
+                long time. Of course, students or students all need it to see the class schedule, so that not 
+                a single morning is forgotten...</p>
+            </div>
+            <div class="individual">
+                <div>
+                <img src="../assets/Ellipse12.png" alt="" />
+                <p className='text-[black] text-[14px]'>Johnny Nguyen</p>
+                </div>
+                <h2 class='content'>Time and motivation</h2>
+                <p className='text-[16px]'>Maybe it's been a long time since I've touched a thing called "timetable". Or more rustic, 
+                people often call it "daily schedule", and for students, it is called "timetable". Surely many 
+                people will wonder why I am a student and have not touched what students call a timetable for a
+                long time. Of course, students or students all need it to see the class schedule, so that not 
+                a single morning is forgotten...</p>
+            </div>
+            <div class="individual">
+                <div>
+                <img src="../assets/Ellipse12.png" alt="" />
+                <p className='text-[black] text-[14px]'>Johnny Nguyen</p>
+                </div>
+                <h2 class='content'>Time and motivation</h2>
+                <p className='text-[16px]'>Maybe it's been a long time since I've touched a thing called "timetable". Or more rustic, 
+                people often call it "daily schedule", and for students, it is called "timetable". Surely many 
+                people will wonder why I am a student and have not touched what students call a timetable for a
+                long time. Of course, students or students all need it to see the class schedule, so that not 
+                a single morning is forgotten...</p>
+            </div>
+        </div>
         </>
     )
 }
