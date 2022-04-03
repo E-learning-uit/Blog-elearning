175e7a57338d603278aaf5c8dc9de6a160f6dd2e
UIT19520358 <19520358@gm.uit.edu.vn>
2022-04-04 02:13:36 +0700

    Lesson seperate 2 columns

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './blogcss/blog.css'
 import Ellipse12 from '../assets/Ellipse12.png'
 
 
@@ -14,12 +13,12 @@ const Blog = () => {
             <h1 className='text-[#003663] text-[31px] py-4 font-bold'>Blog</h1>
             {blog.map(item => (
                 <>
-                    <div className="individual">
-                        <div>
-                            <img src={Ellipse12} alt="" />
-                            <p className='text-[black] text-[14px]'>Johnny Nguyen</p>
+                    <div className="rounded-2xl border-2 border-[#003663] px-4 py-4 hover:border-slate-400">
+                        <div>                            
+                            <p className='text-[black] text-[14px]'>
+                                <img className='inline' src={Ellipse12} alt="" /> Johnny Nguyen</p>
                         </div>
-                        <h2 className='content'>Time and motivation</h2>
+                        <h2 className='text-[#003663] text-[18px] font-semibold py-2'>Time and motivation</h2>
                         <p className='text-[16px]'>Maybe it's been a long time since I've touched a thing called "timetable". Or more rustic,
                             people often call it "daily schedule", and for students, it is called "timetable". Surely many
                             people will wonder why I am a student and have not touched what students call a timetable for a
--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import thumbnail from '../assets/thumbnail_video.png'
 
 
 
@@ -7,8 +9,46 @@ const Lesson = ()=>{
 
     return (
         <>
-
-        <h1>Lesson</h1>
+        <div className="container mx-auto space-y-4">
+            <h1 className='text-[#003663] text-[31px] py-4 font-bold'>Basic programming knowledge</h1>
+            <div class="grid grid-cols-2 gap-2">
+                {/* Cột bên trái */}
+                <div className="">
+                    <video
+                        controls poster={thumbnail}
+                        autoplay
+                        loop
+                        muted
+                        class="absolute inset-0">
+                        <source
+                            src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+                            type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        <h3 className='text-[#003663] text-[24px] py-4'>What is Client-Server?</h3>
+                    </video>
+                </div>
+                {/* Cột bên phải */}
+                <div>
+                    <ul className='text-[#003663] text-[24px] py-4'>I - Technical concepts to know 
+                        <li class='block'>
+                            <video
+                                className='inline w-48'
+                                controls poster={thumbnail}
+                                autoplay
+                                loop
+                                muted>
+                                <source
+                                    src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+                                    type="video/mp4"/>
+                                    Your browser does not support the video tag.
+                            </video>
+                            <p className='text-[#666666] text-[16px] inline'>1. What is Client-Server?</p>
+                        </li>
+                        <li></li>
+                    </ul>            
+                </div>
+            </div>
+        </div>
         </>
     )
 }
